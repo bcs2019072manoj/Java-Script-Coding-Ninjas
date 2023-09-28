c818a5d216c9c7396dcda1952f0194645459904d
Manoj Kumar <117968489+bcs2019072manoj@users.noreply.github.com>
2023-09-28 12:37:26 +0530

    Update Email validation.js

--- Email validation.js
+++ Email validation.js
@@ -1,1 +1,33 @@
-alert("Hello Manoj");
+// Email Validation using and operator
+
+// You have already completed the email validation program using "||" operator.
+
+// Now modify the code and rewrite the conditions to validate the email using the "&&" operator.
+
+
+
+// Objectives
+
+// Additionally, include one more condition that:
+// Email  should have at least 3 characters before “@.”
+// If the email is valid then store the value of email in the result variable with a message.
+
+
+function main(email){
+  
+    let result;
+  
+    let atindex = email.indexOf("@");
+  
+    let dotindex = email.lastIndexOf(".");
+  
+    let len = email.length;
+  
+    if (atindex >= 3 && dotindex - atindex >= 4 && len - dotindex - 1 >= 2) {
+      
+            result = "Welcome " + email + " to our site";
+    } else {
+            result = "invalid email";
+    }
+    return result;
+}
